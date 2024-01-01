94bfd048f3b1f740b0a5790885ae0754a32f33ea
Amrito20 <amrito2025@gmail.com>
2024-01-02 05:14:17 +0600

    function file added.

---
+++ Fundamentals/function.py
@@ -0,0 +1,13 @@
+#function based polymorphisom
+
+def function(data):
+    count =0 
+    for i in data:
+        count= count+1
+    print(count)
+
+dictonary1= {'python':{"easy", "fast", "powerful"}}
+Names= "hello"
+function(Names)
+function(dictonary1)
+
